85b9b0d04825cc94426c239a9d592c17e6e6a549
Michal Zalecki <michal@michalzalecki.com>
2018-04-22 15:16:59 +0200

    Add Counter exercise

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 ## Installation
 
 ```
-npm install truffle@04.1.7 -g
+npm install truffle@4.1.7 -g
 ```
 
 ## Test
 
 ```
 truffle test exercises/hello-world/HelloWorldTest.sol
+truffle test exercises/math-operators/MathOperatorsTest.sol
+truffle test exercises/counter/CounterTest.sol
 ```
---
+++ exercises/counter/Counter.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.4.23;
+
+contract Counter {
+    // declare public count variable (uint) to store number of ticks
+    uint public count = 0; // @remove-line
+
+    // implement tick() to increment counter
+    function tick() public {       // @remove-line
+        count++;                   // @remove-line
+    }                              // @remove-line
+
+    // implement reset() to reset counter back to 0
+    function reset() public {      // @remove-line
+        count = 0;                 // @remove-line
+    }                              // @remove-line
+}
