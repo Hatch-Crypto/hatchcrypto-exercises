bb521466080b5ed8389468691348ced74edb5b34
Michal Zalecki <michal@michalzalecki.com>
2018-04-27 17:43:16 +0200

    Add returning-values exercise

---
+++ exercises/returning-values/Bookshelf.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.4.23;
+
+
+contract Bookshelf {
+    mapping(uint => Book) public books;
+
+    struct Book {
+        string title;
+        uint pages;
+    }
+
+    constructor() public {
+      books[0] = Book({ title: "The Internet of Money", pages: 150 });
+    }
+
+    function getBook(uint _id) view public returns (string, uint) {
+        // look up the book by id and return title and pages using single return instruction
+        Book storage book = books[_id];       // @remove-line
+        return (book.title, book.pages);      // @remove-line
+    }
+
+    function getBookTitle(uint _id) view public returns (string title) {
+        // use getBook and return title without using return explicitly and in a single line
+        (title,) = getBook(_id);              // @remove-line
+    }
+}
