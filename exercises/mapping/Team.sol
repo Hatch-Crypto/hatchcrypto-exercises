78c41126d44c7c1161779cb3bb98e550ceb74c44
Michal Zalecki <michal@michalzalecki.com>
2018-04-30 12:46:57 +0200

    Remove redundant remove-line comment

--- exercises/mapping/Team.sol
+++ exercises/mapping/Team.sol
@@ -11,7 +11,7 @@ contract Team {
     // create setMember for adding a new member that accepts _id, name and position as arguments
     function setMember(uint _id, string _name) public {                   // @remove-line
         members[_id] = Member(_name);                                     // @remove-line
-    }                                                                     // @remove-line
+    }
 
     // create getMemberName that returns name of the member by id
     function getMemberName(uint _id) public view returns (string) {       // @remove-line
