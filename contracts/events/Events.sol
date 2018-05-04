f514864469941ec01249565239407c5268e4ec3c
Michal Zalecki <michal@michalzalecki.com>
2018-05-04 14:33:22 +0200

    Use low-level log1 call to emit an event

--- contracts/events/Events.sol
+++ contracts/events/Events.sol
@@ -5,27 +5,30 @@ import "../iterable-mapping/Airdrop.sol";
 
 contract Events is Airdrop {
     // define AddressAdded(addr) event
-    event AddressAdded(address addr);                         // @remove-line
+    event AddressAdded(address addr);                                            // @remove-line
 
     // define AddressRemoved(addr) event
-    event AddressRemoved(address addr);                       // @remove-line
+    event AddressRemoved(address addr);                                          // @remove-line
 
     // define Airdrop(value, recipientsNum) event
-    event Airdrop(uint value, uint recipientsNum);            // @remove-line
+    event Airdrop(uint value, uint recipientsNum);                               // @remove-line
 
     function addAddress(address _address) public {
         // use super to call addAddress(address) from Airdrop
-        super.addAddress(_address);                           // @remove-line
-        emit AddressAdded(_address);                          // @remove-line
+        // emit AddressAdded event
+        super.addAddress(_address);                                              // @remove-line
+        emit AddressAdded(_address);                                             // @remove-line
     }
 
     function removeAddress(address _address) public {
-        super.removeAddress(_address);                        // @remove-line
-        emit AddressRemoved(_address);                        // @remove-line
+        // emit AddressAdded event using low-level log1 function insteadof emit
+        super.removeAddress(_address);                                           // @remove-line
+        log1(bytes32(_address), bytes32(keccak256("AddressRemoved(address)")));  // @remove-line
     }
 
     function airdrop(uint _value) public {
-        super.airdrop(_value);                                // @remove-line
-        emit Airdrop(_value, addresses.length);               // @remove-line
+        // emit Airdrop event
+        super.airdrop(_value);                                                   // @remove-line
+        emit Airdrop(_value, addresses.length);                                  // @remove-line
     }
 }
