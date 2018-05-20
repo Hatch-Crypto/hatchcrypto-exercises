17d70db599683dc5c0c9494f18eb9ecd6ce78528
Michal Zalecki <michal@michalzalecki.com>
2018-05-20 14:37:35 +0200

    Add more reading material for exercises

--- exercises/first-library/contracts/UseForType.sol
+++ exercises/first-library/contracts/UseForType.sol
@@ -4,7 +4,8 @@ import "./MathOperationsLib.sol";
 
 
 contract UseForType {
-    using MathOperationsLib for uint;
+    // make MathOperationsLib functions available on uint
+    using MathOperationsLib for uint;               // @remove-line
 
     function add(uint _a, uint _b) public pure returns (uint) {
         // implement addition calling add directly on uint variable
--- exercises/third-party-libraries/contracts/UsingSafeMath.sol
+++ exercises/third-party-libraries/contracts/UsingSafeMath.sol
@@ -4,7 +4,8 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 contract UsingSafeMath {
-    using SafeMath for uint256;
+    // make SafeMath functions available on uint256
+    using SafeMath for uint256;              // @remove-line
 
     function sub(uint _a, uint _b) public pure returns (uint) {
         // use sub function provided by SafeMath to throw on underflow
